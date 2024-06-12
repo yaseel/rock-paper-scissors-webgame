6ba2047a5fdc34578c34b1fffdc6297e5aa1a7b2
yaseel <aseelmahmood12@gmail.com>
2024-06-12 13:18:42 +0200

    Commit 2

---
+++ script.js
@@ -0,0 +1,88 @@
+let score = JSON.parse(localStorage.getItem('score')) || {
+    wins: 0,
+    losses: 0,
+    ties: 0
+};
+
+updateScoreElement()
+
+function resetScore() {
+    score.wins = 0;
+    score.losses = 0;
+    score.ties = 0;
+    localStorage.removeItem('score');
+}
+
+function playGame(playerMove) {
+    const computerMove = pickComputerMove();
+
+    let result = '';
+    if (playerMove === 'scissors') {
+        if (computerMove === 'scissors') {
+            result = 'Tie.';
+        } else if (computerMove === 'rock') {
+            result = 'You lose.';
+        } else {
+            result = 'You win.';
+        }
+
+    } else if (playerMove === 'paper') {
+        if (computerMove === 'paper') {
+            result = 'Tie.';
+        } else if (computerMove === 'scissors') {
+            result = 'You lose.';
+        } else {
+            result = 'You win.';
+        }
+
+    } else {
+        if (computerMove === 'rock') {
+            result = 'Tie.';
+        } else if (computerMove === 'paper') {
+            result = 'You lose.';
+        } else {
+            result = 'You win.';
+        }
+    }
+
+    if (result === 'You win.') {
+        score.wins += 1;
+    } else if (result === 'You lose.') {
+        score.losses += 1;
+    } else {
+        score.ties += 1;
+    }
+
+    localStorage.setItem('score', JSON.stringify(score));
+
+    updateScoreElement()
+
+    document.querySelector('.js-result').innerHTML = result;
+
+    document.querySelector('.js-moves')
+        .innerHTML = `You
+          <img class="move-icon" src="images/${playerMove}-emoji.png">
+          <img class="move-icon" src="images/${computerMove}-emoji.png">
+          Computer`
+}
+
+function updateScoreElement() {
+    document.querySelector('.js-score')
+        .innerHTML = `Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`;
+}
+
+function pickComputerMove() {
+    const randomNumber = Math.random();
+
+    let computerMove = '';
+
+    if (randomNumber >= 0 && randomNumber < 1 / 3) {
+        computerMove = 'rock';
+    } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
+        computerMove = 'paper';
+    } else {
+        computerMove = 'scissors';
+    }
+
+    return computerMove;
+}
